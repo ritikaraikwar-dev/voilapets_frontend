5f633177b91533a500fa946ae75a5ae564be990b
ritika_dev <ritikaraikwar24@gmail.com>
2025-10-23 16:32:19 +0530

    .

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -23,22 +23,20 @@ export const CartProvider = ({ children }) => {
   }
   const handleCart = async (id) => {
     try {
+         // Find the full product object from your local products list
+    const product = products.find(item => item.id === id);
 
-       
-      const product = products.find(item => item.id === id);
-
-      const data = await axios.post(`${baseUrl}/voilapets/addCart/${id}`, product, {
-        withCredentials: true,
-      });
-
-
-        
+    // Add quantity field if needed
+    const payload = {
+      ...product,
+      quantity: 1,
+    };
+    const data = await axios.post(`${baseUrl}/voilapets/addCart/${id}`, payload, {
+      withCredentials: true,
+    }); 
     setReloadCart(prev => !prev);
 
       console.log("data send successfully", data.data.userData);
-
-
-
     }
     catch (error) {
       console.log(error);
